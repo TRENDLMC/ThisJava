0df8faa9a2946d3e1a00ea551a977a07694b48f4
DESKTOP-SF6TVD3\503 <trend7936@naver.com>
2023-05-11 18:09:20 +0900

    네트워크자바

---
+++ ch19/sec02/InetAddressExample.java
@@ -0,0 +1,21 @@
+package ch19.sec02;
+
+import java.net.InetAddress;
+
+public class InetAddressExample {
+	public static void main(String[] args) {
+		try {
+			InetAddress local= InetAddress.getLocalHost();
+			System.out.println("내컴퓨터 ip주소는:"+local.getHostAddress());
+			
+			InetAddress[] iaArr= InetAddress.getAllByName("www.naver.com");
+			for (InetAddress remote : iaArr) {
+				System.out.println("www.naver.com IP주소:"+remote.getHostAddress());
+				
+			}
+		} catch (Exception e) {
+		e.printStackTrace();
+		}
+	}
+
+}
